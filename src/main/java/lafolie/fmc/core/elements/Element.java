e281d8691987bda46443f83609f970396ebbb004
Lafolie <lafoliex7@aol.com>
2022-05-25 23:24:52 +0100

    added missing Earth element

--- src/main/java/lafolie/fmc/core/elements/Element.java
+++ src/main/java/lafolie/fmc/core/elements/Element.java
@@ -1,5 +1,8 @@
 package lafolie.fmc.core.elements;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
 /**
  * Enum representing Elemental Aspects
  */
@@ -10,9 +13,25 @@ public enum Element
 	ICE,		
 	LIGHTNING,
 	WIND,	
-	WATER,	
+	WATER,
+	EARTH,	
 	POISON,	
 	HOLY,	
 	DARK,	
 	GRAVITY;
+
+	public static final ImmutableMap<Element, String> TOOLTIP_TEXT = 
+		new ImmutableMap.Builder<Element, String>()
+			.put(Element.NONE, "element.tooltip.none")
+			.put(Element.FIRE, "element.tooltip.fire")
+			.put(Element.ICE, "element.tooltip.ice")
+			.put(Element.LIGHTNING, "element.tooltip.lightning")
+			.put(Element.WIND, "element.tooltip.wind")
+			.put(Element.WATER, "element.tooltip.water")
+			.put(Element.EARTH, "element.tooltip.earth")
+			.put(Element.POISON, "element.tooltip.poison")
+			.put(Element.HOLY, "element.tooltip.holy")
+			.put(Element.DARK, "element.tooltip.dark")
+			.put(Element.GRAVITY, "element.tooltip.gravity")
+			.build();
 }
