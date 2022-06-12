84324fc063b75faaa25651dfd12f03f2a5b8d7ab
Lafolie <lafoliex7@aol.com>
2022-06-12 19:55:55 +0100

    trying to get outline text

--- src/main/java/lafolie/fmc/core/particles/TextBillboardParticle.java
+++ src/main/java/lafolie/fmc/core/particles/TextBillboardParticle.java
@@ -11,6 +11,7 @@ import net.minecraft.client.particle.ParticleFactory;
 import net.minecraft.client.particle.ParticleTextureSheet;
 import net.minecraft.client.particle.SpriteProvider;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -31,7 +32,6 @@ public class TextBillboardParticle extends BillboardParticle
 {
 
 	private OrderedText text = OrderedText.styledForwardsVisitedString("999", Style.EMPTY);
-	// private static TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
 
 	protected TextBillboardParticle(ClientWorld clientWorld, double d, double e, double f)
 	{
@@ -50,7 +50,7 @@ public class TextBillboardParticle extends BillboardParticle
 		collidesWithWorld = false;
 		gravityStrength = 1;
 		velocityY = 5;
-		maxAge = 120;
+		maxAge = 99;
 		// this.
 	}
 
@@ -91,7 +91,6 @@ public class TextBillboardParticle extends BillboardParticle
 
 		TextRenderer renderer = MinecraftClient.getInstance().textRenderer;
 
-		// Quaternion quaternion;
 		Vec3d vec3d = camera.getPos();
 		float x = (float)(MathHelper.lerp((double)tickDelta, this.prevPosX, this.x) - vec3d.getX());
 		float y = (float)(MathHelper.lerp((double)tickDelta, this.prevPosY, this.y) - vec3d.getY());
@@ -105,53 +104,20 @@ public class TextBillboardParticle extends BillboardParticle
 		int color = 0xFFFFFFFF;
 		int outlineColor = 0x00000000;
 
-		// Quaternion quat = camera.getRotation();
-		
 		MatrixStack matStack = new MatrixStack();
 		matStack.push();
 		matStack.translate(x, y, z);
 		matStack.multiply(camera.getRotation());
 
-		VertexConsumerProvider vertConProv = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+		// vertConProv = VertexConsumerProvider.immediate(layerBuffers, fallbackBuffer)
+		// renderer.drawWithShadow(matStack, text, x, y, color);
 
-		renderer.draw(matStack, text, x, y, color);
-		// renderer.drawWithOutline(text, x, y, color, outlineColor, matStack.peek().getPositionMatrix(), vertConProv, Integer.MAX_VALUE);
-
-		// vertexConsumer.vertex(0, 0, 0);
-		// vertexConsumer.vertex(0, 0, 0);
-		// vertexConsumer.vertex(0, 0, 0);
-		// vertexConsumer.vertex(0, 0, 0);
-// public void vertex(float x, float y, float z, float red, float green, float blue, float alpha, float u, float v, int overlay, int light, float normalX, float normalY, float normalZ)
-
+        VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+		renderer.drawWithOutline(text, x, y, color, outlineColor, matStack.peek().getPositionMatrix(), immediate, LightmapTextureManager.MAX_LIGHT_COORDINATE);
+		immediate.draw();
 		
-        // if (this.angle == 0.0f) {
-        //     quaternion = camera.getRotation();
-        // } else {
-        //     quaternion = new Quaternion(camera.getRotation());
-        //     float i = MathHelper.lerp(tickDelta, this.prevAngle, this.angle);
-        //     quaternion.hamiltonProduct(Vec3f.POSITIVE_Z.getRadialQuaternion(i));
-        // }
-        // Vec3f vec3f = new Vec3f(-1.0f, -1.0f, 0.0f);
-        // vec3f.rotate(quaternion);
-        // Vec3f[] vec3fs = new Vec3f[]{new Vec3f(-1.0f, -1.0f, 0.0f), new Vec3f(-1.0f, 1.0f, 0.0f), new Vec3f(1.0f, 1.0f, 0.0f), new Vec3f(1.0f, -1.0f, 0.0f)};
-        // float j = this.getSize(tickDelta);
-        // for (int k = 0; k < 4; ++k) {
-        //     Vec3f vec3f2 = vec3fs[k];
-        //     vec3f2.rotate(quaternion);
-        //     vec3f2.scale(j);
-        //     vec3f2.add(f, g, h);
-        // }
-        // float l = this.getMinU();
-        // float m = this.getMaxU();
-        // float n = this.getMinV();
-        // float o = this.getMaxV();
-        // int p = this.getBrightness(tickDelta);
-        // vertexConsumer.vertex(vec3fs[0].getX(), vec3fs[0].getY(), vec3fs[0].getZ()).texture(m, o).color(this.red, this.green, this.blue, this.alpha).light(p).next();
-        // vertexConsumer.vertex(vec3fs[1].getX(), vec3fs[1].getY(), vec3fs[1].getZ()).texture(m, n).color(this.red, this.green, this.blue, this.alpha).light(p).next();
-        // vertexConsumer.vertex(vec3fs[2].getX(), vec3fs[2].getY(), vec3fs[2].getZ()).texture(l, n).color(this.red, this.green, this.blue, this.alpha).light(p).next();
-        // vertexConsumer.vertex(vec3fs[3].getX(), vec3fs[3].getY(), vec3fs[3].getZ()).texture(l, o).color(this.red, this.green, this.blue, this.alpha).light(p).next();
-    }
-
+		matStack.pop();
+	}
 
 	@Environment(value=EnvType.CLIENT)
 	public static class Factory
